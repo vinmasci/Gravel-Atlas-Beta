267c4fe664e55b2dcbfadd28c040a017756aaa60
vinmasci <vincentmasci@icloud.com>
2024-12-14 15:13:00 +1100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../components/ui/tabs'
 import { DataTable } from '@/components/ui/data-table'
 
 export default function AdminDashboard() {
